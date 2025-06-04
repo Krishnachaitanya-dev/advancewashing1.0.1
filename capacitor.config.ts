634b7e9d23a4f024d5ad4be8b60c5923bccea215
NEO <122967068+Krishnachaitanya-dev@users.noreply.github.com>
2025-06-04 06:18:33 +0000

    Add GitHub Actions workflow to build APK

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,9 @@
-
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.15afaa9a7ebd4807ad5ad06015c9bd36',
-  appName: 'glow-login-palette',
-  webDir: 'dist',
-  server: {
-    url: 'https://15afaa9a-7ebd-4807-ad5a-d06015c9bd36.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  appId: 'com.advancewashing.app',
+  appName: 'Advancewashing',
+  webDir: 'dist'
 };
 
 export default config;
